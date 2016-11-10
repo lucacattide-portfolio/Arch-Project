0155b37ca39fe916c92c9b7d6ebf791df03fe74d
Luca Cattide <luca@laboratorio-a.it>
2016-11-10 16:35:05 +0100

    A&P
    - Template Mail;

---
+++ mail/mail.php
@@ -0,0 +1,125 @@
+<!--Inizio Templte Email-->
+
+<!doctype html>
+
+<!--Inizio HTML-->
+
+<html style="height: 100%; min-height: 100%;">
+
+	<!--Inizio Head-->
+
+<head>
+
+		<!--Inizio Meta-->
+
+		<meta charset="UTF-8"> <!--Codifica Caratteri-->
+
+		<!--Titolo-->
+
+		<title>
+
+			Conferma Registrazione - Arch & Project
+
+		</title>
+
+		<!--Fine Meta-->
+
+		<!--Inizio CSS-->
+
+		<style>
+			
+			/* Webfont: GothamCondensed-Light */@font-face
+			{
+				font-family: 'GothamCondensedLight';
+				src: url('fonts/Gotham Condensed Light/GothamCondensed-Light.eot'); /* IE9 Compat Modes */
+				src: url('fonts/Gotham Condensed Light/GothamCondensed-Light.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */ url('fonts/Gotham Condensed Light/GothamCondensed-Light.woff') format('woff'), /* Modern Browsers */ url('fonts/Gotham Condensed Light/GothamCondensed-Light.ttf') format('truetype'), /* Safari, Android, iOS */ url('fonts/Gotham Condensed Light/GothamCondensed-Light.svg#GothamCondensed-Light') format('svg'); /* Legacy iOS */
+				font-style: normal;
+				font-weight: normal;
+				text-rendering: optimizeLegibility;
+			}
+			
+		</style>
+
+		<!--Fine CSS-->
+
+	</head>
+
+	<!--Fine Head-->
+
+	<!--Inizio Body-->
+
+	<body style="background-color: #1F1F1F; margin: 0; height: 100%; min-height: 100%; font-size: 14px; font-size: 1.1rem;">
+	
+		<!--Inizio Header-->
+	
+		<header style="display: block; position: relative; width: calc(100% - 6em); height: auto; background-color: #000; padding: 1.5em 3em;">
+		
+			<!--Logo-->
+		
+			<aside style="display: block; position: relative; width: 32%; min-width: 147px; max-width: 147px; height: 3em; background-image: url('http://localhost/archeproject/mail/img/logo_mail.svg'); background-size: 100%; background-position: 0 0; background-attachment: scroll; background-repeat: no-repeat;">
+			</aside>	
+		
+		</header>
+		
+		<!--Fine Header-->
+		
+		<!--Inizio Wrapper-->
+		
+		<section style="display: block; position: relative; width: 100%; height: calc(100% - 6em); min-height: calc(100% - 6em); overflow: auto; -webkit-transform-style: preserve-3d;-moz-transform-style: preserve-3d;  transform-style: preserve-3d; margin: 0 auto;">
+		
+			<h7 style="display: none;">
+			
+				Wrapper
+			
+			</h7>
+			
+			<!--Inizio Container-->
+			
+			<div style="display: block; position: relative; top: 50%; width: 100%; max-width: 80%;  transform: translateY(-50%); height: auto; overflow: hidden; background-color: #F2F2F2; border: 5px solid #D8D8D8; padding: 2em; margin: 0 auto; text-align: center;">
+			
+				<!--Inizio Paragrafo-->
+			
+				<p style="margin: 0; color: #95989A; font-family: 'GothamCondensedLight', sans-serif; font-size: 1.1rem; width: auto; display: inline-block; margin: 0 auto; text-align: left;">
+				
+					Buongiorno, <br /><br />Grazie per avere acquistato i nostri prodotti, per scaricare i prodotti del tuo ordine puoi usare il link che trovi qui sotto:<br /><br />
+
+					<a href="#" title="Conferma registrazione" target="new" tabindex="0" style="color: #A00E0E; text-decoration: none;">
+
+						http://www.arch&project.com/index.php?pag=conferma&code=
+
+					</a>
+
+					<br /><br />In alternativa vai su http: //www.arch&project.com/, effettua l’accesso nella sezione <strong style="color: #000;">LOGIN</strong> e clicca su <strong style="color: #000;">ORDINI</strong> per scarica i tuoi acquisti.
+
+
+					<!--Inizio Firma-->
+				
+					<span style="display: block; position: relative; margin-top: 4em;">
+				
+						Cordialmente,<br /><br />Studio ARCH & PROJECT
+					
+					</span>
+					
+					<!--Fine Firma-->
+				
+				</p>
+				
+				<!--Inizio Paragrafo-->
+			
+			</div>
+			
+			<!--Fine Container-->
+		
+		</section>
+		
+		<!--Fine Wrapper-->
+	
+	</body>
+
+	<!--Fine Body-->
+
+</html>
+
+<!--Fine HTML-->
+
+<!--Inizio Templte Email-->
