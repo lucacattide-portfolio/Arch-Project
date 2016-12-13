ae4f0ae3f100259969b2ac863c44c3f21716c868
Luca Cattide <luca@laboratorio-a.it>
2016-12-13 13:06:22 +0100

    A&P
    - Installazione Google Tag Manager;

--- index.php
+++ index.php
@@ -293,6 +293,14 @@ endif;
         <link rel="stylesheet" href="css/style.css"> <!--CSS Main-->
         <link rel="icon" href="favicon.png" type="image/png" /> <!--FavIcon-->
         
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TRX9CHL');</script>
+    <!-- End Google Tag Manager -->
+        
 	</head>
     
     <!--Fine Head-->
@@ -300,6 +308,11 @@ endif;
     <!--Inizio Body-->
 
 	<body>
+           
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRX9CHL"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
             
 	  <?php 
 	  
